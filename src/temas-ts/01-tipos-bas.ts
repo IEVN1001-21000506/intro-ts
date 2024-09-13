603d21ccbcdf217dbc0f21722c154b1fe54e28c3
Cesar Andres Ramirez Garcia <andgg12345@gmail.com>
2024-09-12 19:32:20 -0600

    Primer commit Lunes 9

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,25 @@
+//console.log('Hola Mundo')
+
+var nombre = 'Juan'
+let apellido = 'Perez'
+const edad = 30
+
+let num1=2
+let num2= 5
+let num3= 3
+
+let suma = num1 + num2 + num3
+console.log(suma)
+
+let correo: string;
+let edad2:number;
+let activo:boolean;
+
+correo= 'vcx@gmail.com'
+edad2=21
+activo=true
+
+let matricula: number|string|boolean
+matricula=12345
+matricula='345DF33'
+
